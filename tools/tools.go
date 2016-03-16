63aba2c443976dfa6824a391701d4268563de9df
Lucas Menéndez <hi@lucasmenendez.me>
2016-03-16 18:23:17 +0100

    tools utilities

---
+++ tools/tools.go
@@ -0,0 +1,28 @@
+package tools
+
+import (
+	"encoding/base64"
+	//"encoding/json"
+	"strings"
+)
+
+func BasicAuth(header string) (string, string, bool) {
+	var username, password string
+
+	ok := false
+	if len(header) > 6 {
+		authorization, err := base64.StdEncoding.DecodeString(header[6:])
+		ok = err == nil
+
+		if ok {
+			data := strings.Split(string(authorization), ":")
+			if len(data) < 2 {
+				ok = false
+			} else {
+				username, password = data[0], data[1]
+			}
+		}
+	}
+
+	return username, password, ok
+}
